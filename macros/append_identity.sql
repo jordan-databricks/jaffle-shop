598ab4c884fa21560bde973b616e713128559d39
Jordan Thomas <jordan.thomas@databricks.com>
2024-01-05 00:38:01 -0600

    Overridding macro take 2

--- macros/append_identity.sql
+++ macros/append_identity.sql
@@ -1,3 +1,7 @@
+{% macro databricks__get_incremental_append_sql(arg_dict) %}
+  {% do return(get_insert_into_sql(arg_dict["temp_relation"], arg_dict["target_relation"])) %}
+{% endmacro %}
+
 {% macro get_insert_into_sql(source_relation, target_relation) %}
 
     {%- set dest_columns = adapter.get_columns_in_relation(target_relation) | rejectattr("db_id") -%}
