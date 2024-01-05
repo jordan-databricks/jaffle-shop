699a4b249799c104c2dd6790bf4eccae8ad40810
Jordan Thomas <jordan.thomas@databricks.com>
2024-01-05 02:14:33 -0600

    Changing materialization debug

--- macros/materializations/incremental/strategies.sql
+++ macros/materializations/incremental/strategies.sql
@@ -1,10 +1,10 @@
 {% macro databricks__get_incremental_append_sql(arg_dict) %}
-  {% do return(get_insert_into_sql(arg_dict["temp_relation"], arg_dict["target_relation"])) %}
+  {% do return(get_insert_into_with_identity_sql(arg_dict["temp_relation"], arg_dict["target_relation"])) %}
 {% endmacro %}
 
-{% macro get_insert_into_sql(source_relation, target_relation) %}
+{% macro get_insert_into_with_identity_sql(source_relation, target_relation) %}
 
-    {%- set dest_columns = adapter.get_columns_in_relation(target_relation) | rejectattr("db_id") -%}
+    {%- set dest_columns = adapter.get_columns_in_relation(target_relation) | rejectattr("name", "equalto", "db_id") -%}
     {%- set dest_cols_csv = dest_columns | map(attribute='quoted') | join(', ') -%}
     insert into table {{ target_relation }}
     select {{dest_cols_csv}} from {{ source_relation }}
